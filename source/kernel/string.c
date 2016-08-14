b61d8ba4545efe6da74b6105e1df17f443af1d00
Sharukh Hasan <sharukhhasan@hypercryptic.com>
2016-08-14 05:08:44 -0500

    barrier implementation - Sharukh Hasan

---
+++ source/kernel/string.c
@@ -0,0 +1,180 @@
+/**
+ *
+ * Created by Sharukh Hasan on 8/09/16.
+ * Copyright © 2016 HyperCryptic Solutions, LLC. All rights reserved.
+ *
+ */
+
+#include "kernel/tch_kernel.h"
+#include "kernel/string.h"
+#include <stdarg.h>
+
+#define FORMAT_DELIM			'%'
+#define STR_TERM				'\0'
+
+
+const char NUM_CHAR[] = {
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e','f'
+};
+
+
+void mset(void* dst,int v,size_t sz)
+{
+	if(!dst)
+		return;
+	while(sz--) ((uint8_t*) dst)[sz] = v;
+}
+
+void mcpy(void* dst,const void* src,size_t n)
+{
+	if(!dst || !src)
+		return;
+
+	while(n--)((uint8_t*) dst)[n] = ((uint8_t*) src)[n];
+}
+
+int mcmp(const void* s1,const void* s2,size_t n)
+{
+	if(!s1 || !s2)
+		return -1;
+	size_t i;
+	uint8_t v;
+	for(i = 0;i < n; i++){
+		if(((v = ((uint8_t*) s1)[i] - ((uint8_t*) s2)[i])) != 0)
+			return v < 0? -1 : 1;
+	}
+	return 0;
+}
+
+char* strchar(const char* s,int c)
+{
+	uint32_t idx = 0;
+	while(s[idx] != '\0')
+	{
+		if(s[idx] == c)
+			return (char*) &s[idx];
+		idx++;
+	}
+	if(c == '\0')
+	{
+		return (char*) &s[idx];
+	}
+	return NULL;
+}
+
+size_t strcopy(char* dst,char* src){
+	if(!dst || !src)
+		return 0;
+	char* init_dst = dst;
+	while(*src != '\0'){
+		*(dst++) = *(src++);
+	}
+	return (size_t) (dst - init_dst);
+}
+
+size_t ftostr(float val,char* str,int trunc){
+	if(!str)
+		return 0;
+	float div = 10.f;
+	char* init_str = str;
+	str[0] = NUM_CHAR[(int) val];
+	str[1] = '.';
+	str = &str[2];
+	while(trunc--){
+		val -= (int) val;
+		val *= div;
+		*(str++) = NUM_CHAR[(int) val];
+	}
+	return (size_t) (str - init_str);
+}
+
+size_t itostr(int val,char* str,int radix)
+{
+	if(!str || (radix < 2))
+		return 0;
+	char* init_str = str;
+	if(val < radix)
+	{
+		*str = NUM_CHAR[val];
+		str++;
+		return (size_t) (str - init_str);
+	}
+	int v = val;
+	int div = radix;
+	while(div != 1)
+	{
+		if((v / div) >= radix)
+		{
+			div *= radix;
+		}
+		else
+		{
+			*str = NUM_CHAR[v / div];
+			str++;
+			v %= div;
+			div /= radix;
+		}
+	}
+	*(str++) = NUM_CHAR[v];
+	return (size_t) (str - init_str);
+}
+
+size_t vformat(char* dst,const char* fmt,va_list args){
+	if(!dst)
+		return 0;
+	char *cp,*lcp;
+	size_t sz = 0;
+	uint32_t gap;
+	const char* eos = strchar(fmt,STR_TERM);
+	lcp = cp = (char*) fmt;
+	while((cp < eos) && cp)
+	{
+		cp = (char*) strchar(lcp,FORMAT_DELIM);
+		if(!cp)
+		{
+			sz += strcopy(dst,lcp);
+			return sz;
+		}
+		gap = ((uint32_t) cp - (uint32_t) lcp);
+		sz += gap;
+		mcpy(dst,lcp , ((size_t) cp - (size_t) lcp) * sizeof(char));
+		dst = &dst[gap];
+		lcp = &cp[2];
+		switch(cp[1]){
+		case 'd':
+			dst = &dst[(gap = itostr(va_arg(args,int),dst,10))];
+			sz += gap;
+			break;
+		case 'f':
+			if(cp[2] == '.' && isdigit(cp[3]))
+			{
+				dst = &dst[(gap = ftostr((float) va_arg(args,double),dst,cp[3] - 48))];
+				lcp = &cp[4];
+			}
+			else
+			{
+				dst = &dst[(gap = ftostr((float) va_arg(args,double),dst,5))];
+			}
+			sz += gap;
+			break;
+		case 's':
+			dst = &dst[(gap = strcopy(dst,va_arg(args,char*)))];
+			sz += gap;
+			break;
+		case 'c':
+			*(dst++) = (char) va_arg(args,int);
+			sz++;
+			break;
+		}
+	}
+	return sz;
+}
+
+size_t format(char* dest,const char* fmt,...)
+{
+	if(!dest || !fmt)
+		return 0;
+	va_list args;
+	va_start(args,fmt);
+	return vformat(dest,fmt,args);
+}
---
+++ source/kernel/tch_bar.c
@@ -0,0 +1,182 @@
+/**
+ *
+ * Created by Sharukh Hasan on 8/09/16.
+ * Copyright © 2016 HyperCryptic Solutions, LLC. All rights reserved.
+ *
+ */
+
+
+
+
+/*!\brief Barrier Implementation
+ *
+ *
+ */
+#include "tch.h"
+#include "kernel/tch_kernel.h"
+#include "kernel/tch_bar.h"
+#include "kernel/tch_sched.h"
+#include "kernel/tch_err.h"
+#include "kernel/tch_kobj.h"
+
+#ifndef BARRIER_CLASS_KEY
+#define BARRIER_CLASS_KEY   	     ((uhword_t) 0x2D03)
+#error "might not configured properly"
+#endif
+
+#define BAR_VALIDATE(bar)			do {\
+	((tch_barCb*) bar)->status |= (((uint32_t) bar ^ BARRIER_CLASS_KEY) & 0xFFFF);\
+}while(0)
+
+#define BAR_INVALIDATE(bar)			do {\
+	((tch_barCb*) bar)->status &= ~0xFFFF;\
+}while(0)
+
+#define BAR_ISVALID(bar)			((((tch_barCb*) bar)->status & 0xFFFF) == (((uint32_t) bar ^ BARRIER_CLASS_KEY) & 0xFFFF))
+
+#define BAR_SET_MAX(bar,max)		do {\
+	((tch_barCb*) bar)->status |= ((0xff & max) << 16);\
+}while(0)
+
+#define BAR_GET_MAX(bar)			(0xff & (((tch_barCb*) bar)->status >> 16))
+
+
+__USER_API__ static tch_barId tch_barCreate(uint8_t th_cnt);
+__USER_API__ static tchStatus tch_barEnter(tch_barId bar,uint32_t timeout);
+__USER_API__ static tchStatus tch_barDestroy(tch_barId bar);
+
+static tch_barId bar_init(tch_barCb* bar,uint8_t cnt,BOOL is_static);
+static tchStatus bar_deinit(tch_barCb* bar);
+
+__USER_RODATA__ tch_barrier_api_t Barrier_IX = {
+		.create = tch_barCreate,
+		.enter = tch_barEnter,
+		.destroy = tch_barDestroy
+};
+
+__USER_RODATA__ const tch_barrier_api_t* Barrier = &Barrier_IX;
+
+
+DECLARE_SYSCALL_1(bar_create,uint8_t,tch_barId);
+DECLARE_SYSCALL_2(bar_enter,tch_barId,uint32_t,tchStatus);
+DECLARE_SYSCALL_1(bar_destroy,tch_barId,tchStatus);
+DECLARE_SYSCALL_2(bar_init,tch_barCb*,uint8_t,tchStatus);
+DECLARE_SYSCALL_1(bar_deinit,tch_barCb*,tchStatus);
+
+
+DEFINE_SYSCALL_1(bar_create,uint8_t,th_cnt,tch_barId)
+{
+	tch_barCb* bar = (tch_barCb*) kmalloc(sizeof(tch_barCb));
+	if(!bar)
+		KERNEL_PANIC("can't allocate barrier");
+	bar_init(bar,th_cnt,FALSE);
+	return (tch_barId) bar;
+}
+
+DEFINE_SYSCALL_2(bar_enter,tch_barId,bar,uint32_t,timeout,tchStatus)
+{
+	if((!bar) || (!BAR_ISVALID(bar)))
+		return tchErrorParameter;
+	uint8_t th_max = BAR_GET_MAX(bar);
+	tch_barCb* bcb = (tch_barCb*) bar;
+	bcb->th_cnt++;
+	if(th_max > bcb->th_cnt)
+	{
+		tch_schedWait((tch_thread_queue*)&bcb->wq,timeout);
+	}
+	else
+	{
+		bcb->th_cnt = 0;
+		tch_schedWake((tch_thread_queue*)&bcb->wq,SCHED_THREAD_ALL,tchOK,FALSE);
+	}
+	return tchOK;
+}
+
+DEFINE_SYSCALL_1(bar_destroy,tch_barId,barId,tchStatus)
+{
+	if((!barId) || (!BAR_ISVALID(barId)))
+		return tchErrorParameter;
+	bar_deinit(barId);
+	kfree(barId);
+	return tchOK;
+}
+
+DEFINE_SYSCALL_2(bar_init,tch_barCb*,bp,uint8_t,cnt,tchStatus)
+{
+	if(!bp)
+		return tchErrorParameter;
+	bar_init(bp,cnt,TRUE);
+	return tchOK;
+}
+
+DEFINE_SYSCALL_1(bar_deinit,tch_barCb*,bp,tchStatus)
+{
+	if(!bp || !BAR_ISVALID(bp))
+		return tchErrorParameter;
+	bar_deinit(bp);
+	return tchOK;
+}
+
+static tch_barId bar_init(tch_barCb* bar,uint8_t cnt,BOOL is_static){
+	mset(bar, 0, sizeof(tch_barCb));
+	BAR_VALIDATE(bar);
+	BAR_SET_MAX(bar,cnt);
+	bar->th_cnt = 0;
+	cdsl_dlistEntryInit(&bar->wq);
+	tch_registerKobject(&bar->__obj,is_static? (tch_kobjDestr) tch_barDeinit : (tch_kobjDestr) tch_barDestroy);
+
+	return bar;
+}
+
+static tchStatus bar_deinit(tch_barCb* bar){
+	if((!bar) || (!BAR_ISVALID(bar)))
+		return tchErrorParameter;
+	BAR_INVALIDATE(bar);
+	tch_schedWake((tch_thread_queue*) &bar->wq,SCHED_THREAD_ALL,tchErrorResource,FALSE);
+	tch_unregisterKobject(&bar->__obj);
+	return tchOK;
+}
+
+
+__USER_API__ static tch_barId tch_barCreate(uint8_t cnt)
+{
+	if(tch_port_isISR())
+		return NULL;
+	return (tch_barId) __SYSCALL_0(bar_create);
+}
+
+__USER_API__ static tchStatus tch_barEnter(tch_barId barid,uint32_t timeout)
+{
+	if(!barid)
+		return tchErrorParameter;
+	if(tch_port_isISR())
+		return tchErrorISR;
+	return __SYSCALL_2(bar_enter,barid,timeout);
+}
+
+
+__USER_API__ static tchStatus tch_barDestroy(tch_barId barId)
+{
+	if(!barId)
+		return tchErrorParameter;
+	if(tch_port_isISR())
+		return tchErrorISR;
+	return __SYSCALL_1(bar_destroy,barId);
+}
+
+
+tchStatus tch_barInit(tch_barCb* bar,uint8_t th_cnt){
+	if(!bar)
+		return tchErrorParameter;
+	if(tch_port_isISR())
+		return __bar_init(bar,th_cnt);
+	return __SYSCALL_2(bar_init,bar,th_cnt);
+}
+
+tchStatus tch_barDeinit(tch_barCb* bar){
+	if(!bar)
+		return tchErrorParameter;
+	if(tch_port_isISR())
+		return __bar_deinit(bar);
+	return __SYSCALL_1(bar_deinit,bar);
+}
